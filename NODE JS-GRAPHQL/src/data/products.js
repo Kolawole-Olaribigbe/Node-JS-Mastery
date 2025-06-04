32f399789c227c564407653938dd72b22e0bdcfa
theflyestnerd <kolaolari@gmail.com>
2025-06-04 17:07:13 +0100

    NODE JS-GRAPHQL

--- NODE JS-GRAPHQL/src/data/products.js
+++ NODE JS-GRAPHQL/src/data/products.js
@@ -1,1 +1,25 @@
-cons
+const products = [
+    {
+        id : "1",
+        title : "Laptop",
+        category : "Electronics",
+        price : 990.95,
+        niStock : true
+    },
+    {
+        id : "2",
+        title : "Coffee",
+        category : "Foods",
+        price : 50.95,
+        niStock : true
+    },
+    {
+        id : "3",
+        title : "Shoes",
+        category : "Sports",
+        price : 1990.95,
+        niStock : false
+    },
+];
+
+module.exports = products;
---
+++ NODE JS-GRAPHQL/src/graphql/schema.js
@@ -0,0 +1,43 @@
+//This file determines the structure of your data
+
+const {gql} = require('graphql-tag');
+
+
+//String
+//Int
+//Float
+//Boolean
+//ID -> unique identifier
+
+const typeDefs = gql`
+    type Product {
+        id: ID!
+        title: String!
+        category: String!
+        price: Float!
+        inStock: Boolean!
+    }
+
+    type Query {
+    products: [Product!]!
+    product(id: ID!): Product
+    }
+
+    type Mutation {
+        createProduct(
+            title: String!
+            category: String!
+            price: Float!
+            inStock: Boolean!
+        ) : Product
+        deleteProduct(id: ID!): Boolean
+        updateProduct(
+        id: ID!
+            title: String
+            category: String
+            price: Float
+            inStock: Boolean
+        ): Product
+    }
+`;
+module.exports = typeDefs;
